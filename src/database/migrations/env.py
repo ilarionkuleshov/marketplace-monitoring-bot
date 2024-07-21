0a8a6fdfcd9de4c1588edbe4dcdb5b5d6cd62e45
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-07-21 21:35:15 +0300

    Revert "feat: add Marketplace model and migration"
    This reverts commit 601d288192835b466db58ef1dd5a8c5fc521487e.

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -4,7 +4,6 @@ import alembic_postgresql_enum  # noqa
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from database.models import Base
 from settings import PostgresCredentials
 
 # this is the Alembic Config object, which provides
@@ -20,7 +19,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
