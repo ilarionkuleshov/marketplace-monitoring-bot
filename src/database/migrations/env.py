601d288192835b466db58ef1dd5a8c5fc521487e
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-07-21 20:35:45 +0300

    feat: add Marketplace model and migration

--- src/database/migrations/env.py
+++ src/database/migrations/env.py
@@ -4,6 +4,7 @@ import alembic_postgresql_enum  # noqa
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from database.models import Base
 from settings import PostgresCredentials
 
 # this is the Alembic Config object, which provides
@@ -19,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
