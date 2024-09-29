d9abd9f44d4fea16dc749f0dbf8ad2977c784e4e
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-09-29 20:12:35 +0300

    feat: add PostgresCredentials model

---
+++ src/settings.py
@@ -0,0 +1,24 @@
+from dotenv import find_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PostgresCredentials(BaseSettings):
+    """Credentials for connecting to a PostgreSQL database."""
+
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+    model_config = SettingsConfigDict(env_prefix="postgres_", env_file=find_dotenv(), extra="ignore")
+
+    def get_url(self, async_driver: bool = True) -> str:
+        """Returns the connection URL for the database.
+
+        Args:
+            async_driver (bool): Whether to use an async driver.
+
+        """
+        driver = "asyncpg" if async_driver else "postgresql"
+        return f"{driver}://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
