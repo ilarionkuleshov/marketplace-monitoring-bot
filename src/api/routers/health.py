518b31c63559a73229468cb676df5e8b25f83c10
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-10-29 18:24:10 +0200

    docs: update doc-string

--- src/api/routers/health.py
+++ src/api/routers/health.py
@@ -5,5 +5,10 @@ router = APIRouter(prefix="/health")
 
 @router.get("/")
 async def check_health() -> dict[str, str]:
-    """Checks the health of the API."""
+    """Checks the health of the API.
+
+    Returns:
+        dict[str, str]: A dictionary with the status of the API.
+
+    """
     return {"status": "ok"}
