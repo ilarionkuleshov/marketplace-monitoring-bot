6875dd26c1cf6351d3a64289d8fc140a73e7784c
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-11-04 20:48:49 +0200

    feat: add tasks main script

---
+++ src/tasks/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+import logging
+
+from faststream import FastStream
+from faststream.rabbit import RabbitBroker
+
+from settings import TasksSettings
+
+
+async def main() -> None:
+    """Runs the FastStream application."""
+    settings = TasksSettings()
+
+    broker = RabbitBroker(settings.get_broker_url())
+    app = FastStream(broker)
+
+    await app.run(log_level=logging.getLevelName(settings.log_level))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
