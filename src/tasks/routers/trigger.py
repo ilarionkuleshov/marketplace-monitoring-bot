517c4b375762bf055f782d438d0b93953f88959d
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-11-10 19:37:20 +0200

    feat: add asyncio lock for trigger task

--- src/tasks/routers/trigger.py
+++ src/tasks/routers/trigger.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from faststream import Logger
 from faststream.rabbit import RabbitRouter
 from sqlalchemy import exists, func, not_, or_, select
@@ -12,6 +14,7 @@ from tasks.queues import SCRAPING_TASKS_QUEUE, TRIGGER_SCRAPING_TASKS_QUEUE
 
 router = RabbitRouter()
 scraping_task_publisher = router.publisher(SCRAPING_TASKS_QUEUE, persist=True)
+lock = asyncio.Lock()
 
 
 @router.subscriber(TRIGGER_SCRAPING_TASKS_QUEUE)
@@ -22,58 +25,59 @@ async def handle_trigger_scraping_task(logger: Logger) -> None:
         logger (Logger): FastStream logger.
 
     """
-    async with get_database() as database:
-        monitorings = await database.get_all(
-            model=Monitoring,
-            filters=[
-                Monitoring.enabled.is_(True),
-                not_(
-                    exists(
-                        select(literal(1)).where(
-                            MonitoringRun.monitoring_id == Monitoring.id,
-                            MonitoringRun.status.notin_([MonitoringRunStatus.SUCCESS, MonitoringRunStatus.FAILED]),
+    async with lock:
+        async with get_database() as database:
+            monitorings = await database.get_all(
+                model=Monitoring,
+                filters=[
+                    Monitoring.enabled.is_(True),
+                    not_(
+                        exists(
+                            select(literal(1)).where(
+                                MonitoringRun.monitoring_id == Monitoring.id,
+                                MonitoringRun.status.notin_([MonitoringRunStatus.SUCCESS, MonitoringRunStatus.FAILED]),
+                            )
                         )
-                    )
-                ),
-                or_(
-                    not_(exists(select(literal(1)).where(MonitoringRun.monitoring_id == Monitoring.id))),
-                    (
-                        select(func.max(MonitoringRun.created_at))
-                        .where(MonitoringRun.monitoring_id == Monitoring.id)
-                        .as_scalar()
-                        + Monitoring.run_interval
-                    )
-                    <= func.now(),  # pylint: disable=E1102
-                ),
-            ],
-            read_schema=MonitoringRead,
-        )
-
-        for monitoring in monitorings:
-            await database.create(
-                model=MonitoringRun,
-                data=MonitoringRunCreate(monitoring_id=monitoring.id),
+                    ),
+                    or_(
+                        not_(exists(select(literal(1)).where(MonitoringRun.monitoring_id == Monitoring.id))),
+                        (
+                            select(func.max(MonitoringRun.created_at))
+                            .where(MonitoringRun.monitoring_id == Monitoring.id)
+                            .as_scalar()
+                            + Monitoring.run_interval
+                        )
+                        <= func.now(),  # pylint: disable=E1102
+                    ),
+                ],
+                read_schema=MonitoringRead,
             )
 
-        scraping_tasks = await database.get_all_by_query(
-            query=select(
-                Monitoring.id.label("monitoring_id"),
-                Monitoring.url.label("monitoring_url"),
-                MonitoringRun.id.label("monitoring_run_id"),
+            for monitoring in monitorings:
+                await database.create(
+                    model=MonitoringRun,
+                    data=MonitoringRunCreate(monitoring_id=monitoring.id),
+                )
+
+            scraping_tasks = await database.get_all_by_query(
+                query=select(
+                    Monitoring.id.label("monitoring_id"),
+                    Monitoring.url.label("monitoring_url"),
+                    MonitoringRun.id.label("monitoring_run_id"),
+                )
+                .join(MonitoringRun, MonitoringRun.monitoring_id == Monitoring.id)
+                .where(MonitoringRun.status == MonitoringRunStatus.SCHEDULED),
+                read_schema=ScrapingTask,
             )
-            .join(MonitoringRun, MonitoringRun.monitoring_id == Monitoring.id)
-            .where(MonitoringRun.status == MonitoringRunStatus.SCHEDULED),
-            read_schema=ScrapingTask,
-        )
+
+            for scraping_task in scraping_tasks:
+                await database.update(
+                    model=MonitoringRun,
+                    data=MonitoringRunUpdate(status=MonitoringRunStatus.QUEUED),
+                    filters=[MonitoringRun.id == scraping_task.monitoring_run_id],
+                )
 
         for scraping_task in scraping_tasks:
-            await database.update(
-                model=MonitoringRun,
-                data=MonitoringRunUpdate(status=MonitoringRunStatus.QUEUED),
-                filters=[MonitoringRun.id == scraping_task.monitoring_run_id],
-            )
+            await scraping_task_publisher.publish(scraping_task)
 
-    for scraping_task in scraping_tasks:
-        await scraping_task_publisher.publish(scraping_task)
-
-    logger.info(f"Published {len(scraping_tasks)} scraping tasks")
+        logger.info(f"Published {len(scraping_tasks)} scraping tasks")
