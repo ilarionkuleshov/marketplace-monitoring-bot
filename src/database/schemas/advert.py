88ef6b3686212d382816fc38e91e97776108f66b
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2024-12-31 20:55:10 +0200

    feat(database): update advert model and schemas

--- src/database/schemas/advert.py
+++ src/database/schemas/advert.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import ClassVar
 
 from pydantic import Field
 
@@ -15,6 +16,7 @@ class AdvertRead(DatabaseReadSchema):
 
     id: int
     monitoring_id: int
+    monitoring_run_id: int
     url: str
     title: str
     description: str | None
@@ -22,6 +24,7 @@ class AdvertRead(DatabaseReadSchema):
     price: float | None
     max_price: float | None
     currency: str | None
+    sent_to_user: bool
     created_at: datetime
     updated_at: datetime
 
@@ -30,6 +33,7 @@ class AdvertCreate(DatabaseCreateSchema):
     """Advert schema for creation."""
 
     monitoring_id: int
+    monitoring_run_id: int
     url: StrHttpUrl = Field(max_length=2000)
     title: str = Field(max_length=100)
     description: str | None = Field(default=None, max_length=300)
@@ -38,13 +42,26 @@ class AdvertCreate(DatabaseCreateSchema):
     max_price: float | None = None
     currency: str | None = Field(default=None, min_length=3, max_length=3)
 
+    unique_fields: ClassVar[list[str]] = ["monitoring_id", "url"]
+    update_fields: ClassVar[list[str]] = [
+        "monitoring_run_id",
+        "title",
+        "description",
+        "image",
+        "price",
+        "max_price",
+        "currency",
+    ]
+
 
 class AdvertUpdate(DatabaseUpdateSchema):
     """Advert schema for updating."""
 
+    monitoring_run_id: int | None = None
     title: str | None = Field(default=None, max_length=100)
     description: str | None = Field(default=None, max_length=300)
     image: str | None = Field(default=None, max_length=2000)
     price: float | None = None
     max_price: float | None = None
     currency: str | None = Field(default=None, min_length=3, max_length=3)
+    sent_to_user: bool | None = None
