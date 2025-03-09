9ad3e54fb67e85a175a21f08d3337f67f33b0ac2
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2025-03-09 14:13:13 +0200

    feat: add MARKETPLACE_CRAWLERS_MAPPING

--- src/scrapers/crawlers/__init__.py
+++ src/scrapers/crawlers/__init__.py
@@ -1,3 +1,8 @@
 from .base_advert_crawler import BaseAdvertCrawler
 from .olx_ua_crawler import OlxUaCrawler
 from .shafa_ua_crawler import ShafaUaCrawler
+
+MARKETPLACE_CRAWLERS_MAPPING: dict[str, type[BaseAdvertCrawler]] = {
+    "Olx UA": OlxUaCrawler,
+    "Shafa UA": ShafaUaCrawler,
+}
