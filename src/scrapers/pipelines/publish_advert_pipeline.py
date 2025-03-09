8f008c0b4c3559b4714661000442084ed7f0e3ab
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2025-03-09 14:31:27 +0200

    fix(scrapers): fix method names

--- src/scrapers/pipelines/publish_advert_pipeline.py
+++ src/scrapers/pipelines/publish_advert_pipeline.py
@@ -13,11 +13,11 @@ class PublishAdvertPipeline(BasePipeline):
 
     _broker: RabbitBroker
 
-    async def on_crawler_start(self) -> None:
+    async def on_start(self) -> None:
         """Initializes message broker."""
         self._broker = RabbitBroker(TasksSettings().get_broker_url())
 
-    async def on_crawler_finish(self) -> None:
+    async def on_finish(self) -> None:
         """Closes message broker."""
         await self._broker.close()
 
